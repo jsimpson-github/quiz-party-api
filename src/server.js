8b69e2a02a8d52bf5408358586df539c1dcba772
James Simpson <jsimpson92@hotmail.co.uk>
2020-03-28 14:50:34 +0000

    emit state update globally

--- src/server.js
+++ src/server.js
@@ -1,34 +1,34 @@
-const express = require('express');
-const http = require('http');
-const socketIO = require('socket.io');
-const app = express();
-const server = http.Server(app);
-const io = socketIO(server);
+const express = require('express')
+const http = require('http')
+const socketIO = require('socket.io')
+const app = express()
+const server = http.Server(app)
+const io = socketIO(server)
 
-app.set('port', 5000);
+app.set('port', 5000)
 
 const state = {
     players: []
 }
 
+const join = ({name}) => {
+    state.players.push(name)
+    io.emit('stateUpdated', state)
+}
+
 io.on('connection', function(socket){
-    console.log('a user connected');
-    socket.on('disconnect', function(){
-        console.log('user disconnected');
-    });
-
-    socket.on('join', data => {
-        console.log(data.name)
-        state.players.push(data.name)
-
-        socket.emit('stateUpdated', state)
+    console.log('a user connected')
+    socket.on('disconnect', () => {
+        console.log('user disconnected')
     })
-});
 
-app.get('/ping', function(request, response) {
-    response.send('pong');
-});
+    socket.on('join', join)
+})
 
-server.listen(5000, function() {
-  console.log('Starting server on port 5000');
-});
+app.get('/ping', (request, response) => {
+    response.send('pong')
+})
+
+server.listen(5000, () => {
+  console.log('Starting server on port 5000')
+})
