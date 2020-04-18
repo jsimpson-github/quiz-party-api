e00436d5f9a54f8b2794bca8cfad5154787d99e0
Jennifer Beckingham <jenniferbeckingham@Jennifers-MacBook-Air.local>
2020-04-18 15:54:01 +0100

    Add leave event and apply prettify

--- src/server.js
+++ src/server.js
@@ -1,81 +1,70 @@
-const express = require('express')
-const http = require('http')
-const socketIO = require('socket.io')
-const app = express()
-const server = http.Server(app)
-const io = socketIO(server)
+const express = require("express");
+const http = require("http");
+const socketIO = require("socket.io");
+const app = express();
+const server = http.Server(app);
+const io = socketIO(server);
 
-app.set('port', 5000)
+app.set("port", 5000);
 
 const state = {
     players: [],
-    currentQuestion: null
-}
+    currentQuestion: null,
+};
 
-io.on('connection', function (socket) {
-    console.log('a user connected');
-    socket.on('disconnect', function () {
+io.on("connection", function (socket) {
+    console.log("a user connected");
+    socket.on("disconnect", function () {
         if (true) {
-            console.log('user disconnected');
+            console.log("user disconnected");
         }
     });
 
-    socket.on('join', data => {
+    socket.on("join", (data) => {
         let newPlayer = {
             name: data.name,
-            score: 0
-        }
-        state.players.push(newPlayer)
-        io.emit('stateUpdated', state)
+            score: 0,
+        };
+        state.players.push(newPlayer);
+        io.emit("stateUpdated", state);
     });
 
-    socket.on('ask', ({ name, text }) => {
-        state.currentQuestion = ({ name, text, answers: {} })
-        io.emit('stateUpdated', state)
+    socket.on("ask", ({ name, text }) => {
+        state.currentQuestion = { name, text, answers: {} };
+        io.emit("stateUpdated", state);
     });
 
-    socket.on('answer', ({ name, answer }) => {
-        state.currentQuestion.answers[name] = answer
-        io.emit('stateUpdated', state)
+    socket.on("answer", ({ name, answer }) => {
+        state.currentQuestion.answers[name] = answer;
+        io.emit("stateUpdated", state);
     });
 
-    socket.on('results', ({ results }) => {
+    socket.on("results", ({ results }) => {
         updateScores(results);
         state.currentQuestion = null;
-        io.emit('stateUpdated', state)
+        io.emit("stateUpdated", state);
     });
 
-    io.emit('stateUpdated', state)
+    socket.on("leave", ({ name }) => {
+        state.players = state.players.filter((player) => player.name != name);
+        io.emit("stateUpdated", state);
+    });
+
+    io.emit("stateUpdated", state);
 });
 
-app.get('/ping', (request, response) => {
-    response.send('pong')
-})
+app.get("/ping", (request, response) => {
+    response.send("pong");
+});
 
 server.listen(5000, () => {
-    console.log('Starting server on port 5000')
-})
+    console.log("Starting server on port 5000");
+});
 
 function updateScores(results) {
-    state.players.forEach(player => {
+    state.players.forEach((player) => {
         if (results[player.name]) {
             player.score += results[player.name];
         }
-    })
-    // state.players.map(player => {
-    //     if (results[player.name]) {
-    //         player.score += result[player.name];
-    //     }
-    // })
+    });
 }
-
-// let results = { Jen: '1', James: '0.5' }
-// let players = [{
-//         name: "James",
-//         score: 20
-//     }, 
-//     {
-//         name: "Jen",
-//         score: 5
-//     }
-// ]
