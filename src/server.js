0e945cc53b7c722131f9e1f9bdaa0c6469d0a5fa
Jennifer Beckingham <jenniferbeckingham@Jennifers-MacBook-Air.local>
2020-04-27 21:41:27 +0100

    Have quizzes join channels so only get their own status updates

--- src/server.js
+++ src/server.js
@@ -10,21 +10,28 @@ app.set("port", 5000);
 const state = {};
 
 io.on("connection", function (socket) {
-    console.log("a user connected");
+    id = socket.handshake.query ? socket.handshake.query.id : null;
+    if (id) {
+        socket.join(id);
+        io.to(id).emit("stateUpdated", state[id]);
+    }
+    console.log("user connected");
+
     socket.on("disconnect", function () {
         console.log("user disconnected");
     });
 
-    socket.on("newQuiz", (data) => {
+    socket.on("newQuiz", (data, fn) => {
         id = Object.keys(state).length + 1;
         let quiz = {
             name: data.name,
             id: id,
             players: [],
             currentQuestion: null,
+            active: true,
         };
         state[id] = quiz;
-        socket.emit("quizAdded", id);
+        fn(id);
     });
 
     socket.on("join", ({ id, name }) => {
@@ -33,7 +40,7 @@ io.on("connection", function (socket) {
             score: 0,
         };
         state[id].players.push(newPlayer);
-        io.emit("stateUpdated", state[id]);
+        io.to(id).emit("stateUpdated", state[id]);
     });
 
     socket.on("ask", ({ id, name, text }) => {
@@ -43,34 +50,35 @@ io.on("connection", function (socket) {
             answers: {},
             forceMark: false,
         };
-        io.emit("stateUpdated", state[id]);
+        io.to(id).emit("stateUpdated", state[id]);
     });
 
     socket.on("answer", ({ id, name, answer }) => {
         state[id].currentQuestion.answers[name] = answer;
-        io.emit("stateUpdated", state[id]);
+        io.to(id).emit("stateUpdated", state[id]);
     });
 
     socket.on("results", ({ id, results }) => {
         updateScores(id, results);
         state[id].currentQuestion = null;
-        io.emit("stateUpdated", state[id]);
+        io.to(id).emit("stateUpdated", state[id]);
     });
 
     socket.on("leave", ({ id, name }) => {
         state[id].players = state[id].players.filter(
             (player) => player.name != name
         );
-        io.emit("stateUpdated", state[id]);
+        io.to(id).emit("stateUpdated", state[id]);
     });
 
     socket.on("forceMark", ({ id }) => {
         state[id].currentQuestion.forceMark = true;
-        io.emit("stateUpdated", state[id]);
+        io.to(id).emit("stateUpdated", state[id]);
     });
 
-    socket.on("getState", ({ id }) => {
-        io.emit("stateUpdated", state[id]);
+    socket.on("finish", ({ id }) => {
+        state[id].active = false;
+        io.to(id).emit("stateUpdated", state[id]);
     });
 
     io.emit("connected");
